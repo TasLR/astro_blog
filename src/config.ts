cd3aaee9e750133928bd6e80db00473aea8afcb2
TasLR <fri_lan@163.com>
2024-07-09 11:31:08 +0800

    update name

--- src/config.ts
+++ src/config.ts
@@ -43,7 +43,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
+  name: 'ΔΊγεσΟσο',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
     {
